1e443b2fe63f2f8f3d4dd61ef33576953ac35168
Jamie Wood <woodjamie92@gmail.com>
2016-06-01 14:16:10 +0100

    Added app config

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+// call depenancies and configs ================================================
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// configuration ===============================================================
+// Set View engine to ejs
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static('public'));
+
+// routes ======================================================================
+app.use('/', require('./src/routes/index.js')());
+
+// launch ======================================================================
+app.listen(3001, function (err) {
+  if (err) console.log(err);
+  console.log('running server on port ' + 3002);
+});
