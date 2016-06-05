473d313e6d5e9074428765c1ea1dc5d1bcf36c00
Jamie Wood <woodjamie92@gmail.com>
2016-06-05 12:52:09 +0100

    Added basic functionality to authorise.js

--- src/routes/authorise.js
+++ src/routes/authorise.js
@@ -1,12 +1,45 @@
 'use strict';
 const express = require('express');
 const authoriseRouter = express.Router();
+const request = require('request');
+const username = 'ubnt';
+const password = 'password';
+const uri = 'https://192.168.0.12:8443';
 
 module.exports = function () {
     authoriseRouter.route('/')
         .post(function (req, res) {
-            res.send('test');
-            console.log(req.session);
+            request({
+                method: 'POST',
+                uri: `${uri}/api/login`,
+                json: true,
+                jar: true,
+                body: { 
+                username: username,
+                password: password,
+            },
+                agentOptions: {
+                    rejectUnauthorized: false //Allow Self-signed cert
+                }
+            }, function(err, response, body){
+                console.log(body);
+                request({
+                    method: 'POST',
+                    uri: `${uri}/api/s/default/cmd/stamgr`,
+                    json: true,
+                    jar: true,
+                    body: {
+                        cmd: 'authorize-guest',
+                        mac: req.session.macAddr,
+                    },
+                    agentOptions: {
+                        rejectUnauthorized: false //Allow Self-signed cert
+                    }
+                }, function(err, respsonce, body){
+                    console.log(body);
+                    res.redirect('https://google.co.uk');
+                });
+            });
         });
     return authoriseRouter;
 };
