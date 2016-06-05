4aa50ea229fe0005a61b20721c28755ca5d2c475
Jamie Wood <woodjamie92@gmail.com>
2016-06-05 12:56:38 +0100

    Remove body-parser. Not needed.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const session = require('express-session');
 const app = express();
-const bodyParser = require('body-parser');
 
 // configuration ===============================================================
 // Set View engine to ejs
@@ -11,10 +10,6 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 // Middleware
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
 app.use(express.static('public'));
 app.use(require('express-session')({
   secret: 'verySecretKey',
