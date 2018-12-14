047f852b7d419f1b08be268df10938b1b7c158c1
Jamie Wood <me@jamiewood.io>
2018-12-14 23:57:38 +0000

    Migrate to request-promise

--- src/routes/authorise.js
+++ src/routes/authorise.js
@@ -1,65 +1,43 @@
 const express = require('express');
 const authoriseRouter = express.Router();
-const request = require('request');
+const request = require('request-promise');
 
+let requestOptions = ({
+    method: 'POST',
+    jar: true,
+    json: true,
+    strictSSL: false,
+    uri: "",
+    body: {}
+});
 module.exports = function () {
     authoriseRouter.route('/')
         .post(function (req, res) {
-            request({
-                method: 'POST',
-                uri: `${process.env.URI}/api/login`,
-                json: true,
-                jar: true,
-                body: {
-                    username: process.env.USERNAME,
-                    password: process.env.PASSWORD
-                },
-                agentOptions: {
-                    rejectUnauthorized: false //Allow Self-signed cert
-                }
-            }, function (err, response, body) {
-                if (err) {
-                    console.error(err.stack); 
-                    res.send('An error has occurred');
-                    return; 
-                }
-                console.info(body);
-                request({
-                    method: 'POST',
-                    uri: `${process.env.URI}/api/s/${process.env.SITENAME}/cmd/stamgr`,
-                    json: true,
-                    jar: true,
-                    body: {
-                        cmd: 'authorize-guest',
-                        mac: req.session.macAddr
-                    },
-                    agentOptions: {
-                        rejectUnauthorized: false //Allow Self-signed cert
-                    }
-                }, function (err, response, body) {
-                    if (err) {
-                        console.error(err.stack);
-                        res.send('An error has occurred');
-                        return; 
-                        }
-                    console.info(body);
-                    res.redirect('https://google.co.uk');
-                    request({
-                        method: 'POST',
-                        uri: `${process.env.URI}/logout`,
-                        jar: true,
-                        agentOptions: {
-                            rejectUnauthorized: false //Allow Self-signed cert
-                        }
-                    }, function (err, response, body) {
-                        if (err) {
-                            console.error(err.stack);
-                            return;
-                        }
-                        console.info(body);
-                    });
-                });
-            });
+            requestOptions.uri = `${process.env.URI}/api/login`;
+            requestOptions.body = {
+                username: process.env.USERNAME,
+                password: process.env.PASSWORD
+            };
+            request(requestOptions)
+            .then(loginResp => {
+                requestOptions.uri = `${process.env.URI}/api/s/${process.env.SITENAME}/cmd/stamgr`;
+                requestOptions.body = {
+                    cmd: 'authorize-guest',
+                    mac: "00-14-22-01-23-45"
+                };
+                return request(requestOptions);
+            })
+            .then(authResp => {
+                console.log(authResp);
+                requestOptions.uri = `${process.env.URI}/api/logout`;
+                return request(requestOptions);
+            })
+            .then(logoutResp => {
+                res.redirect('https://google.co.uk');
+            })
+            .catch(err => {
+                console.log(err);
+            })
         });
     return authoriseRouter;
 };
