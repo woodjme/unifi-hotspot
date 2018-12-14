3b2ee7ddc281a1fa598284ce0fcf0a49c7c67de5
Jamie Wood <me@jamiewood.io>
2018-12-14 23:59:31 +0000

    new requestOptions for each request

--- src/routes/authorise.js
+++ src/routes/authorise.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const authoriseRouter = express.Router();
 const request = require('request-promise');
-
-let requestOptions = ({
-    method: 'POST',
-    jar: true,
-    json: true,
-    strictSSL: false,
-    uri: "",
-    body: {}
-});
 module.exports = function () {
     authoriseRouter.route('/')
-        .post(function (req, res) {
-            requestOptions.uri = `${process.env.URI}/api/login`;
-            requestOptions.body = {
-                username: process.env.USERNAME,
-                password: process.env.PASSWORD
-            };
-            request(requestOptions)
+    let requestOptions = ({
+        method: 'POST',
+        jar: true,
+        json: true,
+        strictSSL: false,
+        uri: "",
+        body: {}
+    });
+    .post(function (req, res) {
+        requestOptions.uri = `${process.env.URI}/api/login`;
+        requestOptions.body = {
+            username: process.env.USERNAME,
+            password: process.env.PASSWORD
+        };
+        request(requestOptions)
             .then(loginResp => {
                 requestOptions.uri = `${process.env.URI}/api/s/${process.env.SITENAME}/cmd/stamgr`;
                 requestOptions.body = {
@@ -38,6 +37,6 @@ module.exports = function () {
             .catch(err => {
                 console.log(err);
             })
-        });
+    });
     return authoriseRouter;
 };
