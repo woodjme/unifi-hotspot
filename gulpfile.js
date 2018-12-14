7740d5bb8480d2d433dc2407244fdcbe7db12775
Jamie Wood <me@jamiewood.io>
2018-12-14 21:14:48 +0000

    Remove gulp and bump deps

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const gulpNSP = require('gulp-nsp');
-const path = require('path');
-
-gulp.task('default', ['nsp']);
-
-gulp.task('nsp', function (cb) {
-  gulpNSP({package: path.join(__dirname, '/package.json')}, cb);
-});
