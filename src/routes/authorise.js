efabe316e6dd4bd2449fbd355c95f1425cc67ff5
Jamie Wood <me@jamiewood.io>
2018-12-15 00:00:26 +0000

    new request options for each request

--- src/routes/authorise.js
+++ src/routes/authorise.js
@@ -3,40 +3,40 @@ const authoriseRouter = express.Router();
 const request = require('request-promise');
 module.exports = function () {
     authoriseRouter.route('/')
-    let requestOptions = ({
-        method: 'POST',
-        jar: true,
-        json: true,
-        strictSSL: false,
-        uri: "",
-        body: {}
-    });
-    .post(function (req, res) {
-        requestOptions.uri = `${process.env.URI}/api/login`;
-        requestOptions.body = {
-            username: process.env.USERNAME,
-            password: process.env.PASSWORD
-        };
-        request(requestOptions)
-            .then(loginResp => {
-                requestOptions.uri = `${process.env.URI}/api/s/${process.env.SITENAME}/cmd/stamgr`;
-                requestOptions.body = {
-                    cmd: 'authorize-guest',
-                    mac: "00-14-22-01-23-45"
-                };
-                return request(requestOptions);
-            })
-            .then(authResp => {
-                console.log(authResp);
-                requestOptions.uri = `${process.env.URI}/api/logout`;
-                return request(requestOptions);
-            })
-            .then(logoutResp => {
-                res.redirect('https://google.co.uk');
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    });
+        .post(function (req, res) {
+            let requestOptions = ({
+                method: 'POST',
+                jar: true,
+                json: true,
+                strictSSL: false,
+                uri: "",
+                body: {}
+            });
+            requestOptions.uri = `${process.env.URI}/api/login`;
+            requestOptions.body = {
+                username: process.env.USERNAME,
+                password: process.env.PASSWORD
+            };
+            request(requestOptions)
+                .then(loginResp => {
+                    requestOptions.uri = `${process.env.URI}/api/s/${process.env.SITENAME}/cmd/stamgr`;
+                    requestOptions.body = {
+                        cmd: 'authorize-guest',
+                        mac: "00-14-22-01-23-45"
+                    };
+                    return request(requestOptions);
+                })
+                .then(authResp => {
+                    console.log(authResp);
+                    requestOptions.uri = `${process.env.URI}/api/logout`;
+                    return request(requestOptions);
+                })
+                .then(logoutResp => {
+                    res.redirect('https://google.co.uk');
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        });
     return authoriseRouter;
 };
