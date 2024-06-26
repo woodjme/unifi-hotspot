dc2f3f04c5b1059b805a2ecd94fdc85b47ad4e16
Jamie Wood <jamie@zerobased.co.uk>
2024-06-26 12:08:28 +0100

    add v3 warning

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const port = process.env.PORT || 4545
 // launch
 server.listen(port, function (err) {
   if (!err) {
+    console.log('V3 with breaking changes is coming soon, please pin to v2 or upgrade to v3 when ready.')
     console.log(`running server on port ${port}`)
   } else {
     console.error(err)
